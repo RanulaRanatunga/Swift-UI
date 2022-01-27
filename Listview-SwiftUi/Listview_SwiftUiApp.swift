2d22516e154eaabf929afc54b82c1e8bfe3a6489
Ranula Ranatunga <ranulajanathmax@gmail.com>
2022-01-27 21:33:04 +0530

    Initial Commit

---
+++ Listview-SwiftUi/Listview_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Listview_SwiftUiApp.swift
+//  Listview-SwiftUi
+//
+//  Created by Ranula Ranatunga on 2022-01-27.
+//
+
+import SwiftUI
+
+@main
+struct Listview_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
